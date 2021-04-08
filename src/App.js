19a8c817ee59a0b79f374c0f292e1ea78b2f1103
CosmicTiger <luisangelmarcia@gmail.com>
2021-04-08 04:48:51 -0600

    construction - CosmicTiger - restudying React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import React, { Fragment } from 'react' -> not quite necessary
+
+const App = () => {
+
+    const salute = {
+        name: 'Martin',
+        age: 19
+    }
+
+
+    return (
+        <>
+            { /* <pre>{ JSON.stringify(slute, null, 3) }</pre> */}
+            <p>My first application</p>
+        </>
+    )
 }
 
-export default App;
+export default App
